66eace39ec3229b1717f8fdc18280e409cef38f7
ArshChaudhari <87076283+ArshChaudhari@users.noreply.github.com>
2021-09-15 19:48:07 +0530

    Add files via upload

---
+++ js/Computer.js
@@ -0,0 +1,49 @@
+class Computer {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.life1 = "green";
+    this.life2 = "green";
+    this.life3 = "green";
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+
+    World.add(world, this.body);
+  }
+
+  life() {
+    push()
+     fill("white");
+     textSize(20);
+
+     stroke("black");
+     text("Computer",computer.body.position.x ,computer.body.position.y - 150);
+
+     fill(this.life1)
+     rect(computer.body.position.x - 105, computer.body.position.y - 135, 70, 30);
+
+     fill(this.life1)
+     rect(computer.body.position.x - 35, computer.body.position.y - 135, 70, 30);
+
+     fill(this.life1)
+     rect(computer.body.position.x + 35, computer.body.position.y - 135, 70, 30);
+    pop()
+  }
+  
+   display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
